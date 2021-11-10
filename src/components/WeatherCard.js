38c64b090ca66d251f6a841ea597d9d272830428
ayl2545 <46109675+ayl2545@users.noreply.github.com>
2021-11-10 09:07:38 +0200

    upsate

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -25,13 +25,13 @@ function WeatherCard(props) {
             <h5><Link to='/DailyWeather/' onClick={dayWheathr}>{dateFormat}</Link></h5>
             <img src={getIcon(data.weather_state_abbr)} alt=''/>
             {data.weather_state_name}
-            <h6>max:{Math.round(data.max_temp)}℃</h6>
-            <h6>min:{Math.round(data.min_temp)}℃</h6>
+            <h5>max:{Math.round(data.max_temp)}℃</h5>
+            <h5>min:{Math.round(data.min_temp)}℃</h5>
             <h6>{data.wind_direction_compass} {Math.round(data.wind_speed)} mph</h6>
-            <h5>Humidity {Math.round(data.humidity)}%</h5>
-            <h5>Visibility {(Math.round(data.visibility*10))/10} miles</h5>
-            <h5>Pressure {data.air_pressure}mb</h5>
-            <h5>Confidence {data.predictability}%</h5>
+            <h6>Humidity {Math.round(data.humidity)}%</h6>
+            <h6>Visibility {(Math.round(data.visibility*10))/10} miles</h6>
+            <h6>Pressure {data.air_pressure}mb</h6>
+            <h6>Confidence {data.predictability}%</h6>
             
         </div>
     )
