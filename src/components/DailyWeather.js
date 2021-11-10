ea33edad2f6e60b9a94555e749bab41e9947914f
ayl2545 <46109675+ayl2545@users.noreply.github.com>
2021-11-10 20:15:17 +0200

    update

--- src/components/DailyWeather.js
+++ src/components/DailyWeather.js
@@ -1,12 +1,12 @@
 import React,{useState} from 'react'
 import WeatherCard from './WeatherCard';
-
+import {useNavigate} from 'react-router-dom';
 
 export default function DailyWeather (props){
   const[day]=useState(props.day)
-
+  const navigate = useNavigate();
   const daily=(dailyWeather)=>{
-    
+    navigate(-1)
 }
   
   return(
